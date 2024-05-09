82efbb89bfa3d11b508f569c6d78d0820fe32525
MalenaMontecino <mmontecinom2223@politecnics.barcelona>
2024-05-09 23:55:10 +0200

    czxczxc

---
+++ js/main.js
@@ -0,0 +1,10 @@
+// AOS.init();
+// window.addEventListener('scroll', function () {
+//     var scrollPosition = window.scrollY;
+//     var helloWorld = document.getElementById('helloWorld');
+   
+//     helloWorld.style.fontSize = 16 + scrollPosition / 2 + 'px';
+   
+// });
+
+//CARRUSEL
