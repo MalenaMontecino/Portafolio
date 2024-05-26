159d5146a78707e4a184e1f644e67af8ad2e9ced
MalenaMontecino <mmontecinom2223@politecnics.barcelona>
2024-05-26 22:50:47 +0200

    dfsdfsdfs

--- js/main.js
+++ js/main.js
@@ -9,3 +9,30 @@ $(".custom-carousel").owlCarousel({
     });
   });
  
+  // video interactivo
+  const video = document.getElementById('video');
+  const buttons = document.querySelectorAll('.button');
+
+  function skipTo(seconds) {
+      video.currentTime = seconds;
+      updateButtons(seconds);
+  }
+
+  function updateButtons(seconds) {
+      buttons.forEach(button => button.classList.remove('active'));
+      if (seconds < 10) {
+          document.getElementById('btn1').classList.add('active');
+      } else if (seconds < 20) {
+          document.getElementById('btn2').classList.add('active');
+      } else if (seconds < 30) {
+          document.getElementById('btn3').classList.add('active');
+      } else if (seconds < 40) {
+          document.getElementById('btn4').classList.add('active');
+      } else {
+          document.getElementById('btn5').classList.add('active');
+      }
+  }
+
+  video.addEventListener('timeupdate', () => {
+      updateButtons(video.currentTime);
+  });
